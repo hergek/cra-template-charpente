ef31ec4df9874b1fc61613dec6cd99a3a1243e15
cleverGek <kg.geku@gmail.com>
2022-12-04 18:02:19 +0100

    test

--- template/src/config/axios.js
+++ template/src/config/axios.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: process.env.REACT_APP_BASE_URL });
+const axios = axios.create({ baseURL: process.env.REACT_APP_BASE_URL });
 const me = localStorage.me && JSON.parse(localStorage.getItem("me"));
 const token = me?.token;
 
-api.interceptors.request.use((req) => {
+axios.interceptors.request.use((req) => {
   if (token) {
     req.headers.authorization = token;
   }
   return req;
 });
 
-export default api;
+export const api = axios;
